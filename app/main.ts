e4e2d756ad68f53120b3f33f8a8b827b8dd8ccc0
Pablo Sánchez Flores <pablosanchezflores@gmail.com>
2025-07-21 12:21:26 +0200

    feat: send correlation id

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,18 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+   connection.on("data", () => {
+    // Prepare response buffer (8 bytes total):
+    // 4 bytes for message_size (set to 0 for now)
+    // 4 bytes for correlation_id (set to 7)
+
+    const response = Buffer.alloc(8);
+    response.writeInt32BE(0, 0); // message_size (ignored by tester)
+    response.writeInt32BE(7, 4); // correlation_id
+
+    connection.write(response);
+  });
 });
+
 
 server.listen(9092, "127.0.0.1");
